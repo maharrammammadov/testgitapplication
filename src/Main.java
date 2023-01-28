1cc9759b2fa6ec6381ba168cdbb2a46431fef5fe
Maharram <mmemmedli@hotmail.com>
2023-01-28 10:41:39 +0400

    Hesablama elave ettim

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,13 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Test application");
+
+        int a = 3;
+
+        if(a==3){
+            System.out.println("Reqem  : "+a);
+        }else{
+            System.out.println("Reqem : "+a);
+        }
     }
 }
---
+++ src/Utilits.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Maharram on 1/28/2023.
+ */
+public class Utilits {
+    int check(int a,int b){
+        int c = a+b;
+        return  c;
+    }
+}
