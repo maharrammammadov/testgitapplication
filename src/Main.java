7b4b45303dec8d8f518758b6624f4101ad89874b
Maharram <mmemmedli@hotmail.com>
2023-01-29 19:47:10 +0400

    Yeni test java yaradildi

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Test application");
 
-        int a = 3;
+        int a = 4;
 
         if(a==3){
             System.out.println("Reqem  : "+a);
